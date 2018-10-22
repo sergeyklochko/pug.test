cc65c38a59396866c597157d79730904885e4a83
Sergey <sergeyklochko@gmail.com>
2018-10-22 15:19:53 +0300

    complete kondratenko.ru
    source for condratenko.ru

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
+const concat = require('gulp-concat');
+const del = require('del');
 
 function buildHTML() {
   return gulp.src('./src/*.pug')
   .pipe(pug({
     // Your options in here.
   }))
-  .pipe(gulp.dest('./dest'))
+  .pipe(gulp.dest('./build'))
 }
 
+function buildCSS() {
+  return gulp.src('./src/**/*.css')
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest('./build/css'));
+}
+
+function clean(){
+  return del(['build/*'])
+}
+
+gulp.task('styles', buildCSS);
 gulp.task('pug', buildHTML);
+gulp.task('build', gulp.series(clean, gulp.parallel('styles', 'pug')));
