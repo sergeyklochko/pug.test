2c1efa5cce61a1d0666cb609dc9df71e211b756a
Sergey <sergeyklochko@gmail.com>
2018-10-22 17:05:38 +0300

    add img processing and reorder
    обработка ico

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ const gulp = require('gulp');
 const pug = require('gulp-pug');
 const concat = require('gulp-concat');
 const del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css')
+const flatten = require('gulp-flatten');
+
 
 function buildHTML() {
   return gulp.src('./src/*.pug')
@@ -11,9 +15,32 @@ function buildHTML() {
   .pipe(gulp.dest('./build'))
 }
 
+const imgFiles = [
+  './src/**/*.ico',
+  './src/**/*.jpg'
+];
+
+function buildImages(){
+  return gulp.src(imgFiles)
+  .pipe(flatten())
+  .pipe(gulp.dest('./build/img'))
+}
+
+  const cssFiles = [
+    './node_modules/normalize.css/normalize.css',
+    './src/**/*.css'
+  ];
+
 function buildCSS() {
-  return gulp.src('./src/**/*.css')
+  return gulp.src(cssFiles)
     .pipe(concat('all.css'))
+    .pipe(autoprefixer({
+      browsers: ['> 0.1% '],
+      cascade: false
+    }))
+    .pipe(cleanCSS({
+      level: 2
+    }))
     .pipe(gulp.dest('./build/css'));
 }
 
@@ -23,4 +50,5 @@ function clean(){
 
 gulp.task('styles', buildCSS);
 gulp.task('pug', buildHTML);
-gulp.task('build', gulp.series(clean, gulp.parallel('styles', 'pug')));
+gulp.task('img', buildImages);
+gulp.task('build', gulp.series(clean, gulp.parallel('styles', 'pug', 'img')));
