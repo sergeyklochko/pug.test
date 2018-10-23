395d87effccfc2bdc363103c104a76371a169dbd
Sergey <sergeyklochko@gmail.com>
2018-10-23 10:23:28 +0300

    fix for destination
    fix for destination

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css')
 const flatten = require('gulp-flatten');
 
-const destdir = './docs';
+var destdir = './build';
 
 function buildHTML() {
   return gulp.src('./src/*.pug')
@@ -33,6 +33,7 @@ function buildImages(){
   ];
 
 function buildCSS() {
+  console.log(destdir);
   return gulp.src(cssFiles)
     .pipe(concat('all.css'))
     .pipe(autoprefixer({
@@ -45,13 +46,30 @@ function buildCSS() {
     .pipe(gulp.dest(destdir+'/css'));
 }
 
+function publish(){
+  destdir = './docs';
+  clean();
+  buildCSS();
+  buildHTML();
+  buildImages();
+  return console.log(destdir);
+}
+
+function buildProject(){
+  clean();
+  buildCSS();
+  buildHTML();
+  buildImages();
+}
+
 function clean(){
   return del([destdir+'/*'])
 }
 
-gulp.task('styles', buildCSS);
-gulp.task('pug', buildHTML);
-gulp.task('img', buildImages);
-gulp.task('build', gulp.series(clean, gulp.parallel('styles', 'pug', 'img')));
+gulp.task('styles', buildCSS); // process CSS
+gulp.task('pug', buildHTML); //process HTML
+gulp.task('img', buildImages); // process images
+gulp.task('build', buildProject); // build for local test
+gulp.task('publish', publish) // publish it to GitPages
 
 // 123
